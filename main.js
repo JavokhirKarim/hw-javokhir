2212b0ee264d5c188cbbba90f7962cca7b0ac679
unknown <javohirkarimiy@gmail.com>
2023-09-27 09:35:04 +0500

    changed

--- main.js
+++ main.js
@@ -69,15 +69,67 @@
 //  If the string contains an odd number of characters then it should replace the missing
 //  second character of the final pair with an underscore ('_').
 
-let str = "abcdefg";
-var pairs = [];
+// let str = "abcdefg";
+// var pairs = [];
 
-for (var i = 0; i < str.length; i += 2) {
-  var pair = str.slice(i, i + 2);
-  if (pair.length === 1) {
-    pair += "_";
-  }
-  pairs.push(pair);
+// for (var i = 0; i < str.length; i += 2) {
+//   var pair = str.slice(i, i + 2);
+//   if (pair.length === 1) {
+//     pair += "_";
+//   }
+//   pairs.push(pair);
+// }
+
+// console.log(pairs);
+
+////7-masala
+// Check to see if a string has the same amount of 'x's and 'o's.
+// /The method must return a boolean and be case insensitive. The string can contain any char.
+
+// let str = "xxoxoo";
+// let arr = str.split("");
+// let once = arr.filter((el, tot) => el == tot);
+// console.log(once);
+
+////8-masala
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// let arr = ["Ryan", "Kieran", "Mark"];
+// let newArr = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i].length === 4) {
+//     newArr.push(arr[i]);
+//   }
+// }
+// console.log(newArr);
+
+////9-masala
+// In a small town the population is p0 = 1000 at the beginning of a year.
+//  The population regularly increases by 2 percent per year and moreover 50 new inhabitants per
+// year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+
+// let p0 = 1000;
+// let p = 1200;
+// let count = 0;
+
+// while (p0 < p) {
+//   count++;
+//   p0 += (p0 * 2) / 100 + 50;
+// }
+// console.log(count);
+
+///9-masala
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+// let pin = "1234";
+// let neww = /^[0-9]{4}$|^[0-9]{6}$/.test(pin);
+// console.log(neww);
+
+///10-masaal
+let str = "abcd";
+for (let i = 0; i < str.length; i++) {
+  let arr = str.split("");
+  console.log(arr[i]);
 }
-
-console.log(pairs);
